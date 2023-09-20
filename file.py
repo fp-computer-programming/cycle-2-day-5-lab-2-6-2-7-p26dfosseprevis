2319a783b3422904969c4297bea0fc2bc1dcf590
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-20 14:35:14 -0400

    finished work

--- file.py
+++
@@ -1,8 +0,0 @@
-# Create a Python file named lab_2-6.py
-
-# Set a = 5, b = 10, c = 0, d = -4
-
-# Create a conditional statement to determine if each variable is even or odd. 
- 
-# Print the results.
-
